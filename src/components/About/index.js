e91ca40240600783faf072f06fc6529f4f13b743
Connor Phillips <cmphillips23@gmail.com>
2021-09-22 01:18:57 -0500

    updated about me section and fixed resume not showing up on page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
         <section>
             <h2>About Me</h2>
             <p className='about'>
-                I am an aspiring software developer who is currently enrolled in the Full Stack Web Developer Boot Camp through the University of Texas.  I have 4 years of management experience in the transportation industry, and am ready for a career change.  I am able to translate my teamwork skills and dedication to customer satisfaction into the world of coding, where successful collaboration is key to any larger project's success, and clients are always setting a high standard for results.
+                I am an aspiring software developer, and I just completed the Full Stack Web Developer Boot Camp through the University of Texas.  I have 4+ years of management experience in the transportation industry, and I am ready to explore opportunities in the world of technology and software programming.  My career so far has required me to think critically and make decisions in high-stress situations, and also to be responsible for all facets of the operations to which I was assigned.  I believe that my experience so far has given me the tools necessary to succeed in any work environment, and I am ready for the next challenge that comes my way!
             </p>
         </section>
     )
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ResumePic from '../../images/resume.PNG'
 
 function Resume() {
     return (
-        <section className="resume">
-            
+        <section>
+            <img alt={"Resume"} src={ResumePic} height="100%" width="100%" />
         </section>
     )
 }
